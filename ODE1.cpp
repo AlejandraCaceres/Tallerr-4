6dd4a7212da454aaa771bf8a016ba73ddbb91e93
AlejandraCaceres <42219621+AlejandraCaceres@users.noreply.github.com>
2018-11-16 16:17:02 -0500

    Add files via upload
    Punto ecuacion movimiento proyectil

---
+++ ODE1.cpp
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include <math.h>
+#include "iostream"
+#include "fstream"
+using namespace std;
+
+
+// declarar variables
+float c=0.2;
+float g=10.0;
+float m=0.2;
+float tiempo_total=3.0;
+int n_tiempo=5000;
+float instante_t=tiempo_total/(n_tiempo-1);
+
+
+void leapfrog_ode(float beta, float velocidad_inicial, char *nombre_archivo)
+{
+	// crear vectores
+	float x[n_tiempo];
+	float y[n_tiempo];
+	float xdot;
+	float ydot;
+	float vx[n_tiempo];
+	float vy[n_tiempo];
+	float vxdot;
+	float vydot;
+
+	ofstream archivo_salida(nombre_archivo);
+
+	// valores iniciales
+	x[0] = 0.0;
+	vx[0] = velocidad_inicial*cos(beta);
+	y[0] = 0.0;
+	vy[0] = velocidad_inicial*sin(beta);
+
+	// Avanzar las velocidades medio instante de tiempo
+	vxdot = -vx[0]*c/m*( vx[0]*vx[0] + vy[0]*vy[0] ) /sqrt(vx[0]*vx[0] + vy[0]*vy[0]);
+	vydot = -g -vy[0]*c/m*( vx[0]*vx[0] + vy[0]*vy[0] ) /sqrt(vx[0]*vx[0] + vy[0]*vy[0]);
+	vx[0] = vx[0] + 0.5*instante_t*vxdot;
+	vy[0] = vy[0] + 0.5*instante_t*vydot;
